15d90d07313d3eade9dd31cb78c5aec5d32d62a5
Nicolas Pedernera <nicopedernera12@gmail.com>
2021-01-20 07:14:03 -0300

    Update geoload.py

--- geoload.py
+++ geoload.py
@@ -12,7 +12,7 @@ api_key = False
 
 if api_key is False:
     api_key = 42
-    serviceurl = "http://py4e-data.dr-chuck.net/json?"
+    serviceurl = "http://py4e-data.dr-chuck.net/geojson?"
 else :
     serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
 
